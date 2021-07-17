bcfe195afbadf5baab6a8400e33eb95672750372
pranjal singh <suryakantsharma.me@gmail.com>
2021-07-17 21:18:07 +0530

    remove unnecessary files

--- src/components/Action/index.js
+++
@@ -1,8 +0,0 @@
-const UserState = (state) => {
-    return {
-        type: "USERSTATE",
-        state: state
-    }
-}
-
-export default UserState;
--- src/components/Reduser/index.js
+++
@@ -1,7 +0,0 @@
-import Reducer from './reducer';
-import { combineReducers } from 'redux';
-const rootReducer = combineReducers({
-    Reducer
-})
-
-export default rootReducer;
